df6fc409054a832df47dc01ce4dd333ccfa7bc22
HabibaSajid321 <habibachudhary434@gmail.com>
2023-08-21 10:30:17 +0500

    first comit

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+// TypeScript 1
+var message = "Hey Abu Hurrairah, I have started learing TYPESCRIPT";
+console.log(message);
+// storing my name 
+var myName = "Habiba Sajid";
+console.log("My name is " + "" + myName);
+// store 10 numbers in var
+var num1 = 2;
+var num2 = 5;
+var num3 = 7;
+var num4 = 0;
+var num5 = 29;
+var num6 = 6;
+var num7 = 1;
+var num8 = 10;
+var num9 = 26;
+var num10 = 13;
+// sum of all numbers
+var sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("sum of all numbers is  " + " " + sum);
+// difference
+var diff = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("difference of all numbers is  " + " " + diff);
+// multiplication
+var mul = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("multiplication of all numbers is  " + " " + mul);
+// division
+var div = num8 / num1;
+console.log("division of 2 numbers is " + " " + div);
+let a = 10;
+console.log(a);
---
+++ index.ts
@@ -0,0 +1,43 @@
+// TypeScript 1
+var message:string = "Hey Abu Hurrairah, I have started learing TYPESCRIPT";
+console.log(message);
+
+// storing my name 
+var myName:string = "Habiba Sajid";
+console.log("My name is " + "" + myName);
+
+// store 10 numbers in var
+
+var num1:number = 2;
+var num2:number = 5;
+var num3:number = 7;
+var num4:number = 0;
+var num5:number = 29;
+var num6:number = 6;
+var num7:number = 1;
+var num8:number = 10;
+var num9:number = 26;
+var num10:number = 13;
+// sum of all numbers
+var sum:number = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+console.log("sum of all numbers is  " + " "+ sum );
+
+// difference
+var diff:number = num1-num2-num3-num4-num5-num6-num7-num8-num9-num10;
+console.log("difference of all numbers is  " + " "+ diff );
+
+// multiplication
+
+var mul:number = num1*num2*num3*num4*num5*num6*num7*num8*num9*num10;
+console.log("multiplication of all numbers is  " + " "+ mul );
+
+
+// division
+var div:number = num8/num1;
+console.log("division of 2 numbers is " + " " + div);
+
+
+
+let a = 10;
+console.log(a);
+ 
